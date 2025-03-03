33135348c7bdeaac43bd52bcd28bb59d1b657909
Sunny Dodti <sunnydodti@gmail.com>
2025-03-04 02:24:15 +0530

    password generator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pearpin/data/theme_provider.dart';
+import 'package:pearpin/service/password_service.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,12 +12,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    _generatePassword();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: _buildAppBar(),
-      body: Column(
-        children: [Text("gbfn")],
+      body: Container(
+        color: Theme.of(context).colorScheme.background,
+        child: Container(
+          padding: EdgeInsets.all(24),
+          color: Theme.of(context).colorScheme.background,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildPasswordBox(),
+              SizedBox(
+                height: 20,
+              ),
+              _buildRefreshButton(),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -25,15 +56,47 @@ class _HomePageState extends State<HomePage> {
         ? Icon(Icons.light_mode_outlined)
         : Icon(Icons.dark_mode_outlined);
     return AppBar(
-      title: Text("Currency Exchange"),
+      title: Text("PearPin"),
       centerTitle: true,
       actions: [
         IconButton(
-            onPressed: () {
-              context.read<ThemeProvider>().toggleTheme();
-            },
+            onPressed: () => context.read<ThemeProvider>().toggleTheme(),
             icon: icon),
       ],
+    );
+  }
+
+  Widget _buildPasswordBox() {
+    return Row(
+      children: [
+        Expanded(
+            child: TextField(
+          controller: _passwordController,
+          readOnly: true,
+        )),
+        IconButton(
+          icon: Icon(Icons.copy_outlined),
+          onPressed: () {
+            Clipboard.setData(ClipboardData(text: _passwordController.text));
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Copied to clipboard"),
+            ));
+          },
+        )
+      ],
+    );
+  }
+
+  _generatePassword() {
+    String password = PasswordService.generatePassword();
+    setState(() => _passwordController.text = password);
+  }
+
+  Widget _buildRefreshButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+          onPressed: _generatePassword, child: Icon(Icons.refresh_outlined)),
     );
   }
 }
